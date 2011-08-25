bfda901cc7ca353209c166b335b1639c7003e717
hotsw4p <hotsw4p@gmail.com>
2011-08-25 14:09:34 -0500

    initial commit for rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Invasion::Application.config.secret_token = '7256afc6ff331410eddb6def33da2d06a8dcca269cdc9e20b8a95b777bf745e7c6086ea25b29a32cf583d8336d2ebe91a835c2d2a06845ef09d198b3602bb2b0'
